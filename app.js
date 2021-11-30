d5f482961fa2ca8b04bf4dd16090702ffb975d67
subhayan <subhayan.m5@gmail.com>
2021-11-30 12:23:22 +0530

    fixing bug: mongoose-findorcreate-case sensitive

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const session = require('express-session');
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const findOrCreate = require('mongoose-findOrCreate');
+const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
 
