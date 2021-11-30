1bcc83fd325cf505c6d38d096eb91099a68f48cb
subhayan <subhayan.m5@gmail.com>
2021-11-30 11:54:10 +0530

    Prepare app for Heroku

--- app.js
+++ app.js
@@ -175,6 +175,8 @@ app.post("/login", function(req, res){
 });
 
 
-app.listen(3000, function(){
-  console.log("server started at port 3000");
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
